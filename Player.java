4d2e4c06de3580ccc4db227712cd2129512505ed
elsadeuxpancera <elsa.deuxpancera@gmail.com>
2026-04-02 09:07:22 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -7,13 +7,13 @@ public class Player {
    private int currentX;
    private int currentY;
 
-
    public Player(int health, String playerName, int startingX, int startingY) {
       // set the health and playerName to the passed parameters
       this.health = health;
       this.playerName = playerName;      
       
       // TODO: create the empty Inventory
+      inventory = new ArrayList<Item>();
       
       // TODO: set the startingX and Y of the player
       this.currentX = startingX;
@@ -22,28 +22,41 @@ public class Player {
 
    public void addItemToInventory(Item item) {
       // TODO: add an item to inventory
+      inventory.add(item);
    }
    
    public Item removeItemFromInventory(Item item) {
       // TODO: remove an item to inventory. Return the Item so it can returned to the Room that it is in
+      inventory.remove(item);
+      return item;
    }
-   
    
    public boolean hasItem(String itemName) {
       return false;
    }
    
-   
    public String look(Map map) {
       // TODO: This method will take the player's current X and Y, and return the contents of the room
       // they are in with a descriptive String
       // HINT: Use map.getRoom(currentX, currentY) and call toString()
+      return map.getRoom(currentX, currentY).toString();
    }
    public void moveTo(int x, int y) {
       // TODO: Update the Player's currentX and currentY values.
+      this.currentX = x;
+      this.currentY = y;
    }
    
    public Room getRoom(Map map) {
       // TODO: return the room object that the player is currently in, given the Map that was passed
+      return map.getRoom(currentX, currentY);
+   }
+   
+   public static void main(String[] args) {
+      Player p = new Player(100, "Hero", 0, 0); 
+      Map map = new Map(5, 6); 
+      System.out.println(p.look(map)); 
+      p.moveTo(1, 2); 
+      System.out.println(p.look(map));
    }
 }
